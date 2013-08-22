b682dc4c621deec5a58a8940a5bd03724d095af7
aclement <aclement@10.80.32.2>
2013-08-22 08:51:15 -0600

    Demo text game

---
+++ src/demo/DemoTextGame.java
@@ -0,0 +1,25 @@
+package demo;
+
+import java.util.Scanner;
+
+public class DemoTextGame {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Welcome to the GAME OF LIFE!!!!!!");
+
+		while (true) {
+			System.out.println("Are you ready to play? (y/n)");
+			String input = s.nextLine();
+			if (input.equals("y")) {
+				System.out.println("You are born! Congrats!");
+			} else if (input.equals("n")) {
+				System.out.println("Go play outside.");
+				System.exit(0);
+			} else {
+				System.out.println("Your amibiguity confuses me.");
+			}
+		}
+
+	}
+}
