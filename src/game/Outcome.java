f49d9835cdb1729fd915f3cebe2ef0d52934c80d
WesleyDencker <WesleyDencker@10.0.1.7>
2013-08-23 19:58:04 -0600

    Added outcome.

---
+++ src/game/Outcome.java
@@ -0,0 +1,32 @@
+package game;
+/**
+ * 
+ * @author wdencker
+ *
+ */
+
+
+
+public class Outcome {
+	private int choiceList;
+	private int choiceNumber;
+	private boolean canDo;
+	/**
+	 * 
+	 * @param choiceList is the number associated with the group of choices the player is
+	 * presented with - determined in the Choices class.
+	 * @param choiceNumber is the number of the choice in the choice list
+	 * that is selected by the user and determined in the Choices class
+	 * @param canDo is a boolean that indicates whether the character can do the selected
+	 * decision successfully (determined in Choices class based off stats in the Person class
+	 */
+	
+	//canDo is a boolean - also determined in choices class - that indicates whether the 
+	//character can do the 
+	public Outcome(int choiceList, int choiceNumber, boolean canDo) {
+		this.choiceList = choiceList;
+		this.choiceNumber = choiceNumber;
+		this.canDo = canDo;
+	}
+
+}
---
+++ src/game/Starter.java
@@ -0,0 +1,25 @@
+package game;
+
+/**
+ * @author aclement
+ * 
+ */
+public class Starter {
+
+	/**
+	 * getFive is a class designed to return the number 5
+	 * 
+	 * @return the integer 5
+	 */
+	public int getFive() {
+		return 5;
+	}
+
+	/**
+	 * @return the String "Hi"
+	 */
+	public String produceString() {
+		return "Hi";
+	}
+
+}
