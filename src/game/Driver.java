dcf3686f6471ce95ebfd35b7e9f2a473b56dd66f
jpoley15 <jpoley15@10.80.9.101>
2013-08-26 13:08:46 -0600

    Added comments

--- src/game/Driver.java
+++ src/game/Driver.java
@@ -6,6 +6,7 @@ public class Driver {
 
 	public static void main(String[] args) {
 		
+		/////////// Makes new Person with name a ///////////
 		
 		Scanner s = new Scanner(System.in);
 		System.out.println("Welcome to Decisions");
@@ -13,9 +14,11 @@ public class Driver {
 		String a = s.nextLine();
 		Person p = new Person(a);
 		
+		/////////// Outputs stats generated randomly upon creation of Person p //////////
+		
 		System.out.println("These are your stats:");
 		System.out.println(p.getName() + ":");
-		System.out.println("You are a man");
+		System.out.println("You are makle");
 		System.out.println("You are currently a child");
 		System.out.println("Charisma = " + p.getCharisma());
 		System.out.println("Intelligence = " + p.getIntelligence());
