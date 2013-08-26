46a7f76d5c02d22b4101691ba5ff5289a9806c8f
cwilson14 <cwilson14@192.168.1.142>
2013-08-26 13:37:59 -0600

    Added isQualified to ChoiceStorage, it'll error out until Choice has required attributes and getters

--- src/game/ChoiceStorage.java
+++ src/game/ChoiceStorage.java
@@ -1,14 +1,38 @@
 package game;
 
+import java.util.ArrayList;
+
 public class ChoiceStorage {
 
-	private Choice[] choices = new Choice[10];
+	private ArrayList<Choice> choices = new ArrayList<Choice>();
 
 	/**
 	 * Builds a new instance of ChoiceStorages
 	 */
 	public ChoiceStorage() {
-
+		
+	}
+	
+	private boolean isQualified(Person p, Choice c){
+		if(!(p.getAge()==c.getAge())){
+			return false;
+		}
+		else if(!(p.getCharisma()>=c.getCharisma())){
+			return false;
+		}
+		else if(!(p.getConfidence()>=c.getConfidence())){
+			return false;
+		}
+		else if(!(p.getIntelligence()>=c.getIntelligence())){
+			return false;
+		}
+		else if(!(p.getStrength()>=c.getStrength())){
+			return false;
+		}
+		else if(!(p.getWealth()>=c.getWealth())){
+			return false;
+		}
+		return true;
 	}
 
 	/**
