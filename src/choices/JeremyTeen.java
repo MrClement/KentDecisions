bc9696c95f34ccc1469185ed7e081258b3e20d91
jpoley15 <jpoley15@10.80.9.101>
2013-08-28 11:32:53 -0600

    Worked on JeremyTeen

--- src/choices/JeremyTeen.java
+++ src/choices/JeremyTeen.java
@@ -4,12 +4,39 @@ import game.Choice;
 
 public class JeremyTeen {
 	
-	Choice c1 = new Choice(printText1, storylines, outcomelines, age, charisma, intelligence, strength, wealth, confidence);
-	Choice c2 = new Choice(printText1, storylines, outcomelines, age, charisma, intelligence, strength, wealth, confidence);
-	Choice c3 = new Choice(printText1, storylines, outcomelines, age, charisma, intelligence, strength, wealth, confidence);
-	Choice c4 = new Choice(printText1, storylines, outcomelines, age, charisma, intelligence, strength, wealth, confidence);
-	Choice c5 = new Choice(printText1, storylines, outcomelines, age, charisma, intelligence, strength, wealth, confidence);
-	Choice c6 = new Choice(printText1, storylines, outcomelines, age, charisma, intelligence, strength, wealth, confidence);
-	Choice c7 = new Choice(printText1, storylines, outcomelines, age, charisma, intelligence, strength, wealth, confidence);
+	private String pt1 = "";
+	private String pt2 = "";
+	private String pt3 = "";
+	private String pt4 = "";
+	private String pt5 = "";
+	
+	private String[] sl1 = {
+		""
+	};
+	
+	private String[] sl2 = {
+		""
+	};
+	
+	private String[] sl3 = {
+		""
+	};
+	
+	private String[] sl4 = {
+			""
+	};
+	
+	private String[] sl5 = {
+		""
+	};
+	
+	
+	
+	Choice c1 = new Choice(pt1, sl1, outcomelines, age, charisma, intelligence, strength, wealth, confidence);
+	Choice c2 = new Choice(pt2, sl2, outcomelines, age, charisma, intelligence, strength, wealth, confidence);
+	Choice c3 = new Choice(pt3, sl3, outcomelines, age, charisma, intelligence, strength, wealth, confidence);
+	Choice c4 = new Choice(pt4, sl4, outcomelines, age, charisma, intelligence, strength, wealth, confidence);
+	Choice c5 = new Choice(pt5, sl5, outcomelines, age, charisma, intelligence, strength, wealth, confidence);
+	
 	
 }
