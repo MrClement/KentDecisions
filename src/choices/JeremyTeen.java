8b5b29d60b036a31edbee8bb0d0f5c3fb94a3b96
jpoley15 <jpoley15@10.80.9.101>
2013-08-28 11:21:12 -0600

    created choices

--- src/choices/JeremyTeen.java
+++ src/choices/JeremyTeen.java
@@ -1,5 +1,15 @@
 package choices;
 
-public class JeremyTeen {
+import game.Choice;
 
+public class JeremyTeen {
+	
+	Choice c1 = new Choice(printText1, storylines, outcomelines, age, charisma, intelligence, strength, wealth, confidence);
+	Choice c2 = new Choice(printText1, storylines, outcomelines, age, charisma, intelligence, strength, wealth, confidence);
+	Choice c3 = new Choice(printText1, storylines, outcomelines, age, charisma, intelligence, strength, wealth, confidence);
+	Choice c4 = new Choice(printText1, storylines, outcomelines, age, charisma, intelligence, strength, wealth, confidence);
+	Choice c5 = new Choice(printText1, storylines, outcomelines, age, charisma, intelligence, strength, wealth, confidence);
+	Choice c6 = new Choice(printText1, storylines, outcomelines, age, charisma, intelligence, strength, wealth, confidence);
+	Choice c7 = new Choice(printText1, storylines, outcomelines, age, charisma, intelligence, strength, wealth, confidence);
+	
 }
