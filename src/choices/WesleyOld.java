f77a9d1346aec4d7d6946192e66e73b7ca91dd28
WesleyDencker <WesleyDencker@10.80.9.186>
2013-08-29 09:37:54 -0600

    Added baseline constructors and instance variables to WesleyOld - now working on storyboarding and will wait to do choices until after Choice class is rewritten

--- src/choices/WesleyOld.java
+++ src/choices/WesleyOld.java
@@ -1,9 +1,15 @@
 package choices;
 
+import game.Outcome;
 import game.Person;
 
 public class WesleyOld {
 	private Person p;
+	
+	public WesleyOld(Person p){
+		this.p = p;
+	}
+	
 	private String pt1 = "";
 	private String pt2 = "";
 	private String pt3 = "";
@@ -40,5 +46,25 @@ public class WesleyOld {
 			"D"
 		};
 	
+	// ************************* //
+	//         Outcomes         //
+	
+	//private Outcome pt1c1 = new Outcome(alive, charisma, intelligence, strength, wealth, confidence, age);
+	//private Outcome pt1c2 = new Outcome(alive, charisma, intelligence, strength, wealth, confidence, age);
+	//private Outcome pt1c3 = new Outcome(alive, charisma, intelligence, strength, wealth, confidence, age);
+	//private Outcome pt1c4 = new Outcome(alive, charisma, intelligence, strength, wealth, confidence, age);
+	//private Outcome pt2c1 = new Outcome(alive, charisma, intelligence, strength, wealth, confidence, age);
+	//private Outcome pt2c2 = new Outcome(alive, charisma, intelligence, strength, wealth, confidence, age);
+	//private Outcome pt2c3 = new Outcome(alive, charisma, intelligence, strength, wealth, confidence, age);
+	//private Outcome pt2c4 = new Outcome(alive, charisma, intelligence, strength, wealth, confidence, age);
+	//private Outcome pt3c1 = new Outcome(alive, charisma, intelligence, strength, wealth, confidence, age);
+	//private Outcome pt3c2 = new Outcome(alive, charisma, intelligence, strength, wealth, confidence, age);
+	//private Outcome pt3c3 = new Outcome(alive, charisma, intelligence, strength, wealth, confidence, age);
+	//private Outcome pt3c4 = new Outcome(alive, charisma, intelligence, strength, wealth, confidence, age);
+	//private Outcome pt4c1 = new Outcome(alive, charisma, intelligence, strength, wealth, confidence, age);
+	//private Outcome pt4c2 = new Outcome(alive, charisma, intelligence, strength, wealth, confidence, age);
+	//private Outcome pt4c3 = new Outcome(alive, charisma, intelligence, strength, wealth, confidence, age);
+	//private Outcome pt4c4 = new Outcome(alive, charisma, intelligence, strength, wealth, confidence, age);
+
 	
 }
