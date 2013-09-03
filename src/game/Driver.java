cee451d448bbb09e3ffac340848413c5584904eb
cwilson14 <cwilson14@192.168.1.142>
2013-09-03 08:39:15 -0600

    Changed driver to make dying easy and made the choice selector make sense (1=1 instead of 1=0)

--- src/game/Driver.java
+++ src/game/Driver.java
@@ -45,11 +45,14 @@ public class Driver {
 		/////////// Runs the Game ///////////
 		
 		while (p.isAlive() == true){ 
-		c = cs.getNextChoice(p); // Makes c the next choice
 		c.print();  // Outputs the choice to the user 
 		int x = s.nextInt(); // Takes in the user's input and stores it in x
 		o=c.execute(x);  // Makes decision with x
 		o.updateAttributes(p); // Updates the players Attributes
+		if(!p.isAlive()){
+			break;
+		}
+		c = cs.getNextChoice(p); // Makes c the next choice
 		}
 		
 		/////////// Prints Final stats ///////////
