845ad540e60c5c5e7b7f45c57804cf8a4f057f02
King <hjames14@kentdenver.org>
2013-09-10 09:20:53 -0600

    Finished

--- src/choices/HarryYouth.java
+++ src/choices/HarryYouth.java
@@ -85,7 +85,7 @@ public class HarryYouth {
 	String [] c3Story = new String []{
 			"You and your bros go outside hit on some ladies and P some Ls. \nYou kids are clearly the kings of the castle keep it up",
 			"Your dedication to academia is apparaent. \nYour teacher loves you and you feel your brain growing",
-			"you again.....",
+			"You again.....",
 			"You guys play some tag and hide and go seek keep growing with your peers."
 			};
 	
@@ -116,10 +116,77 @@ public class HarryYouth {
 	//Choice4
 	String [] c4Story = new String[]{
 			"Your pretty much the Cleaver family. \nYou have family dinners and movie nights on the weekend, your parents grant you a ton of responsility and push you to new heights",
-			""
-	}
+			"Wow your personality is really showing now... needless to say your life is pretty boring",
+			"Your parents hate you but the kids respect you. You snek out on the reg with the rest of your boys teepeing and smoking tea. You and your boys are the talk of the town, boys want to be you and girls want their nap mat next to yours. lets just hope your parents wont retaliate that much.",
+			"Good choice bro. Your birthdays are filled with Vinard Vines boxes and Brookes Brothers bags. Your country club lifestyle has made you the talk of the school. THe boys want to be you and the girls want their nap mat next to you. May your croakies hang with pride...",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"HAHA you think YOU could be a rebel?? Your parents laugh and joke about your 'phases' and the kids around school think your more of a tool than you already are.",
+			"Although your intentions shape what you will strive for in your life...your family's wealth is no where near that level."
+	};
 	
-	Choice c4 = new Choice("You have come of age to choose what your family life will be like. \n1.Be a happy content boy that always does what their parents say. \n2.Stay in your room and study and obey your parents whenever. \n3.Your a rebel...Enough said. \n4.You follow in the excessive and bourgeois lifestyle of your parents", storylines, outcomelines, age, charisma, intelligence, strength, wealth, confidence);
-	Choice c5 = new Choice(printText1, storylines, outcomelines, age, charisma, intelligence, strength, wealth, confidence);
+	//boolean alive, int charisma, int intelligence, int strength, int wealth, int confidence, int age) 
+	Outcome [] outcomeLineC4 = new Outcome []{
+			new Outcome(true, 5, 1, 0, 0, 2, 0),
+			new Outcome(true, -2, 5, -1, 0, -2, 0),
+			new Outcome(true, 3, -2, 0, -1, 5, 0),
+			new Outcome(true, 3, 0, 0, 2, 6, 0),
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			new Outcome(true, 0, 0, 0, 0, -5, 0),
+			new Outcome(true, 2, 0, 0, 0, 1, 0)
+	};
+	
+	//int charisma, int intelligence, int strength, int wealth, int confidence
+	Requirements r4 [] =  {
+			new Requirements(0, 0, 0, 0, 0),
+			new Requirements(-1,-1,-1,-1,-1),
+			new Requirements(0,0,0,0,5),
+			new Requirements(0,0,0,5,0),
+	};
+	
+	Choice c4 = new Choice("You have come of age to choose what your family life will be like. \n1.Be a happy content boy that always does what their parents say. \n2.Stay in your room and study and obey your parents whenever. \n3.Your a rebel...Enough said. \n4.You follow in the excessive and bourgeois lifestyle of your parents", 
+			c4Story, r4, outcomeLineC4, d, d.getAge(), d.getCharisma(), d.getIntelligence(), d.getStrength(), d.getWealth(), d.getConfidence());
+	
+	//Choice5
+	
+	String [] c5Story = new String[]{
+			"As you approach the shorts you can hear your destiny calling...Your choice is the generic.",
+			"As you approach the shorts you can hear your destiny calling...Your one of the cool kids.",
+			"As you approach the shorts you can hear your destiny calling...You are a prep master, born to join a frat.",
+			"As you approach the shorts you can hear your destiny calling...You are an artsy hipster.",
+			"As you approach the shorts you can hear your destiny calling...You are a happy hippie."
+	};
+	//boolean alive, int charisma, int intelligence, int strength, int wealth, int confidence, int age) 
+	Outcome [] outcomeLineC5 = new Outcome []{
+			new Outcome(true, 3, 2, 0, 0, 2, 1),
+			new Outcome(true, 3, 0, 0, 0, 5, 0),
+			new Outcome(true, 3, 0, 0, 1, 5, 0),
+			new Outcome(true, 4, 3, 0, 0, 0, 0),
+			new Outcome(true, 6, 0, 0, 0, 0, 0)
+	};
+	
+	Requirements r5[] = {
+			new Requirements(0, 0, 0, 0, 0),
+			new Requirements(0, 0, 0, 0, 0),
+			new Requirements(0, 0, 0, 0, 0),
+			new Requirements(0, 0, 0, 0, 0),
+			new Requirements(0, 0, 0, 0, 0),
+	};
+	
+	Choice c5 = new Choice("Now here is the BIG decision...\nYour parents have finally have decided to let you choose your pants...\n(Be Careful and wise this pretty much determines the game).\n1.Choose cargo shorts.\n2.Choose jeans.\n3.Choose Nantucket Reds\4.Cordaroy Overalls.\n5.Tie Die Shorts.", c5Story, r5, outcomeLineC5,
+			d, d.getAge(), d.getCharisma(), d.getIntelligence(), d.getStrength(), d.getWealth(), d.getConfidence());
 
 }
